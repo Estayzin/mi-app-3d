f2226a6402cece89316c5780cdd3b55663cab452
Carlos <carlos@geisterconsultores.com>
2026-03-20 19:02:08 -0300

    "IfcLoader-y-FragmentsManager"

--- src/main.js
+++ src/main.js
@@ -1,23 +1,6 @@
-import * as THREE from "three";
 import Stats from "stats.js";
-import * as OBC from "@thatopen/components";
-import * as OBF from "@thatopen/components-front";
 import * as BUI from "@thatopen/ui";
-
-BUI.Manager.init();
-
-// Esperar a que bim-grid esté definido como custom element
-await customElements.whenDefined("bim-grid");
-
-const grid = document.getElementById("grid");
-grid.layouts = {
-  main: `
-    "header header" auto
-    "sidebar content" 1fr
-    / auto 1fr
-  `
-};
-grid.render("main");
+import * as OBC from "@thatopen/components";
 
 const components = new OBC.Components();
 const worlds = components.get(OBC.Worlds);
@@ -28,13 +11,14 @@ world.scene.setup();
 world.scene.three.background = null;
 
 const container = document.getElementById("container");
-world.renderer = new OBF.PostproductionRenderer(components, container);
+world.renderer = new OBC.SimpleRenderer(components, container);
 world.camera = new OBC.OrthoPerspectiveCamera(components);
-await world.camera.controls.setLookAt(68, 23, -8.5, 21.5, -5.5, 23);
+await world.camera.controls.setLookAt(78, 20, -2.2, 26, -4, 25);
 
 components.init();
-world.renderer.postproduction.enabled = true;
+components.get(OBC.Grids).create(world);
 
+// FragmentsManager
 const githubUrl = "https://thatopen.github.io/engine_fragment/resources/worker.mjs";
 const fetchedUrl = await fetch(githubUrl);
 const workerBlob = await fetchedUrl.blob();
@@ -44,13 +28,6 @@ const fragments = components.get(OBC.FragmentsManager);
 fragments.init(workerUrl);
 
 world.camera.controls.addEventListener("update", () => fragments.core.update());
-
-world.onCameraChanged.add((camera) => {
-  for (const [, model] of fragments.list) {
-    model.useCamera(camera.three);
-  }
-  fragments.core.update(true);
-});
 
 fragments.list.onItemSet.add(({ value: model }) => {
   model.useCamera(world.camera.three);
@@ -66,18 +43,49 @@ fragments.core.models.materials.list.onItemSet.add(({ value: material }) => {
   }
 });
 
-const fragPaths = [
-  "https://thatopen.github.io/engine_components/resources/frags/school_arq.frag",
-];
-await Promise.all(
-  fragPaths.map(async (path) => {
-    const modelId = path.split("/").pop()?.split(".").shift();
-    if (!modelId) return null;
-    const file = await fetch(path);
-    const buffer = await file.arrayBuffer();
-    return fragments.core.load(buffer, { modelId });
-  }),
-);
+// Cargar fragments
+const loadFragments = async () => {
+  const fragPaths = [
+    "https://thatopen.github.io/engine_components/resources/frags/school_arq.frag",
+    "https://thatopen.github.io/engine_components/resources/frags/school_str.frag",
+  ];
+  await Promise.all(
+    fragPaths.map(async (path) => {
+      const modelId = path.split("/").pop()?.split(".").shift();
+      if (!modelId) return null;
+      const file = await fetch(path);
+      const buffer = await file.arrayBuffer();
+      return fragments.core.load(buffer, { modelId });
+    }),
+  );
+};
+
+// Exportar fragments
+const downloadFragments = async () => {
+  for (const [, model] of fragments.list) {
+    const fragsBuffer = await model.getBuffer(false);
+    const file = new File([fragsBuffer], `${model.modelId}.frag`);
+    const link = document.createElement("a");
+    link.href = URL.createObjectURL(file);
+    link.download = file.name;
+    link.click();
+    URL.revokeObjectURL(link.href);
+  }
+};
+
+// Eliminar modelos
+const deleteArchModel = () => {
+  const modelIds = [...fragments.list.keys()];
+  const modelId = modelIds.find((key) => /arq/.test(key));
+  if (!modelId) return;
+  fragments.core.disposeModel(modelId);
+};
+
+const deleteAllModels = () => {
+  for (const [modelId] of fragments.list) {
+    fragments.core.disposeModel(modelId);
+  }
+};
 
 // Stats
 const stats = new Stats();
@@ -89,3 +97,72 @@ const statsLabel = stats.dom.querySelector("div:last-child");
 if (statsLabel) statsLabel.style.display = "none";
 world.renderer.onBeforeUpdate.add(() => stats.begin());
 world.renderer.onAfterUpdate.add(() => stats.end());
+
+// UI
+BUI.Manager.init();
+
+const [panel, updatePanel] = BUI.Component.create((_) => {
+  const onLoadFragments = async ({ target }) => {
+    target.loading = true;
+    await loadFragments();
+    target.loading = false;
+  };
+
+  let loadFragmentsBtn;
+  if (fragments.list.size === 0) {
+    loadFragmentsBtn = BUI.html`
+      <bim-button label="Load fragments" @click=${onLoadFragments}></bim-button>
+    `;
+  }
+
+  let disposeArchModelBtn;
+  if ([...fragments.list.keys()].some((key) => /arq/.test(key))) {
+    disposeArchModelBtn = BUI.html`
+      <bim-button label="Dispose Arch Model" @click=${deleteArchModel}></bim-button>
+    `;
+  }
+
+  let downloadFragmentsBtn;
+  let disposeModelsBtn;
+  if (fragments.list.size > 0) {
+    disposeModelsBtn = BUI.html`
+      <bim-button label="Dispose All Models" @click=${deleteAllModels}></bim-button>
+    `;
+    downloadFragmentsBtn = BUI.html`
+      <bim-button label="Export fragments" @click=${downloadFragments}></bim-button>
+    `;
+  }
+
+  return BUI.html`
+    <bim-panel active label="FragmentsManager Tutorial" class="options-menu">
+      <bim-panel-section label="Controls">
+        ${loadFragmentsBtn}
+        ${disposeArchModelBtn}
+        ${disposeModelsBtn}
+        ${downloadFragmentsBtn}
+      </bim-panel-section>
+    </bim-panel>
+  `;
+}, {});
+
+const updateFunction = () => updatePanel();
+fragments.list.onItemSet.add(updateFunction);
+fragments.list.onItemDeleted.add(updateFunction);
+
+document.body.append(panel);
+
+const button = BUI.Component.create(() => {
+  return BUI.html`
+    <bim-button class="phone-menu-toggler" icon="solar:settings-bold"
+      @click="${() => {
+        if (panel.classList.contains("options-menu-visible")) {
+          panel.classList.remove("options-menu-visible");
+        } else {
+          panel.classList.add("options-menu-visible");
+        }
+      }}">
+    </bim-button>
+  `;
+});
+
+document.body.append(button);
